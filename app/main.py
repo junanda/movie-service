9a011c4e7cf2a5ba20c36176540e04c8cddd62b7
anzoid <jpatihullah@gmail.com>
2021-07-17 14:19:41 +0700

    make api for service movie

--- app/main.py
+++ app/main.py
@@ -1,35 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
+from api.movie import movies
 
 app = FastAPI()
 
-# Create Dummy Data
-
-fake_movie_db = [
-    {
-        'name': "Start Wars: Episode IX - The Rise of Skywalker",
-        "plot": "The surviving members of the resistance face the First Order once again.",
-        "genres": ['Action', 'Adventure', 'Fantasy'],
-        "casts": ['Daisy Ridley', 'Adam Driver']
-    }
-]
-
-
-class Movie(BaseModel):
-    name: str
-    plot: str
-    genres: List[str]
-    casts: List[str]
-
-
-@app.get("/", response_model=List[Movie])
-async def index():
-    return fake_movie_db
-
-
-@app.post('/', status_code=201)
-async def add_movie(payload: Movie):
-    movie = payload.dict()
-    fake_movie_db.append(movie)
-    return {'id': len(fake_movie_db) - 1}
+app.include_router(movies)
